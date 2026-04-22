a1c270f5f62e9b21c88f351b360e72ceaa492733
W33r4kk0d1 <thejanranasingheofc@gmail.com>
2026-04-22 13:23:26 +1200

    Delete_Fix

---
+++ F1_DataAccess/Repository/IRepository/IManufacturerRepository.cs
@@ -0,0 +1,15 @@
+﻿using F1_Garage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1_DataAccess.Repository.IRepository
+{
+    public interface IManufacturerRepository:IRepository<Manufacturer>
+    {
+        void Update(Manufacturer obj);
+        void Save();
+    }
+}
--- F1_Garage/Controllers/ManufacturerController.cs
+++ F1_Garage/Controllers/ManufacturerController.cs
@@ -1,32 +1,110 @@
 ﻿using F1_Garage.Data;
+using F1_Garage.Models;
 using Microsoft.AspNetCore.Mvc;
+using F1_DataAccess.Repository.IRepository;
+using System.Reflection.Metadata.Ecma335;
 
 namespace F1_Garage.Controllers
 {
     public class ManufacturerController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public ManufacturerController(ApplicationDbContext db)
+        private readonly IManufacturerRepository _manufacturerRepo;
+        public ManufacturerController(IManufacturerRepository db)
         {
-            _db = db;
+            _manufacturerRepo = db;
         }
         public IActionResult Index()
         {
-            var objManufacturerList= _db.Manufacturers.ToList();
+            List<Manufacturer> objManufacturerList = _manufacturerRepo.GetAll().ToList();
             return View(objManufacturerList);
-        }
-
-        [HttpPost]
-        public IActionResult Create(Models.Manufacturer obj)
-        {
-            _db.Manufacturers.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Models.Manufacturer obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display cannot be the same as the Name!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _manufacturerRepo.Add(obj);
+                _manufacturerRepo.Save();
+                TempData["Success"] = "New Manufacturer added successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // EDIT BUTTON
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            Manufacturer? manufacturerFromDb = _manufacturerRepo.Get(u=>u.Id == id);
+
+            if(manufacturerFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(manufacturerFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Manufacturer obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _manufacturerRepo.Update(obj);
+                _manufacturerRepo.Save();
+                TempData["Success"] = "Manufacturer details have been updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Delete Button
+        public IActionResult Delete(int? id)
+        {
+            Manufacturer? obj = _manufacturerRepo.Get(u => u.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Manufacturer? obj = _manufacturerRepo.Get(u => u.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _manufacturerRepo.Remove(obj);
+            _manufacturerRepo.Save();
+            TempData["Success"] = "Manufacturer details have been deleted successfully!";
+            return RedirectToAction("Index");
+        }
     }
 }
+
+
