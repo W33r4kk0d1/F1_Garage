1f9e4c239e07f9550df731a215d36f05d33a192b
W33r4kk0d1 <thejanranasingheofc@gmail.com>
2026-04-22 13:36:40 +1200

    program.cs_class

--- F1_Garage/Program.cs
+++ F1_Garage/Program.cs
@@ -3,32 +3,39 @@ using F1_DataAccess.Repository.IRepository;
 using F1_Garage.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IManufacturerRepository, ManufacturerRepository>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace F1_Garage
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    public static class AppConfig
+    {
+        // SERVICES
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IManufacturerRepository, ManufacturerRepository>();
+        }
+
+        public static void ConfigureApp(WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
