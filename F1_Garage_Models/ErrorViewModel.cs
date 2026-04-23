991f14b066896e3ea869f54e184c35cf72db1609
W33r4kk0d1 <thejanranasingheofc@gmail.com>
2026-04-24 01:32:48 +1200

    Delete F1_Garage_Models directory

--- F1_Garage_Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace F1_Garage.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
